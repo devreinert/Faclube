21852b628f002b8f40855b26f14a452a23c79c0c
guilherme.reinert <gui.moop7@gmail.com>
2025-04-02 21:29:44 -0300

    Adicionado campo sexo

---
+++ js/site.js
@@ -0,0 +1,23 @@
+function validaFaleConosco(){
+    if(document.frmCadastro.txtname.value==""){
+        alert("Preencha o campo Nome. ");
+        document.frmCadastro.txtname.focus();
+        return false;
+    }
+    if(document.frmCadastro.txtfone.value==""){
+        alert("Preencha o campo Telefone. ");
+        document.frmCadastro.txtfone.focus();
+        return false;
+    }
+    if(document.frmCadastro.txtemail.value==""){
+        alert("Preencha o campo E-mail. ");
+        document.frmCadastro.txtemail.focus();
+        return false;
+    }
+    if(document.frmCadastro.CheckMasc.value==""){
+        alert("Informe seu sexo. ");
+        document.frmCadastro.CheckMasc.focus();
+        return false;
+    }
+    return true;
+}
