3cb61188ae247f27218409659a5fed1b1c0d6e35
guilherme.reinert <gui.moop7@gmail.com>
2025-04-07 21:00:10 -0300

    Fim OT8

--- js/site.js
+++ js/site.js
@@ -19,11 +19,45 @@ function validaFaleConosco(){
         document.frmCadastro.CheckMasc.focus();
         return false;
     }
-    if(document.comentario.txtcomentario.value=""){
+    if(document.frmCadastro.selMotivo.value==""){
         alert("Preencha o motivo. ");
-        document.comentario.txtcadastro.focus();
+        document.frmCadastro.selMotivo.focus();
         return false;
     }
     return true;
 }
+
+function verificaMotivo(motivo){
+    var elemento = document.getElementById("opcaoProduto");
+
+    
+    if(motivo=="SI"){
+        
+        var select = document.createElement("select");
+       
+        select.setAttribute("name", "selMotivo");
+        var option = document.createElement("option");
+        option.setAttribute("value", "");
+        var texto = document.createTextNode("Escolha");
+        option.appendChild(texto);
+        select.appendChild(option);
+        
+
+        var option = document.createElement("option");
+        option.setAttribute("value", "FR");
+        var texto = document.createTextNode("Descontos");
+        option.appendChild(texto);
+        select.appendChild(option);
+
+        elemento.appendChild(select);
+    }
+        
+        else{
+            if(elemento.firstChild)
+                elemento.removeChild(elemento.firstChild);
+            
+            }
+
+}
+
 
